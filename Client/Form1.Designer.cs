a30168810c96047902d236d2387878e51f83b1cf
Do Nguyen <daniel@zincer.com>
2015-11-14 10:14:44 +0700

    Applied Minimax

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConnect = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.bgwReceiveData = new System.ComponentModel.BackgroundWorker();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblFlag = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(191, 72);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(81, 23);
+            this.btConnect.TabIndex = 0;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(21, 82);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(38, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Wait...";
+            // 
+            // bgwReceiveData
+            // 
+            this.bgwReceiveData.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwReceiveData_DoWork);
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(21, 15);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(20, 13);
+            this.lblIP.TabIndex = 3;
+            this.lblIP.Text = "IP:";
+            // 
+            // lblFlag
+            // 
+            this.lblFlag.AutoSize = true;
+            this.lblFlag.Location = new System.Drawing.Point(12, 49);
+            this.lblFlag.Name = "lblFlag";
+            this.lblFlag.Size = new System.Drawing.Size(30, 13);
+            this.lblFlag.TabIndex = 4;
+            this.lblFlag.Text = "Flag:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(47, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(225, 20);
+            this.txtIP.TabIndex = 5;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(47, 46);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(225, 20);
+            this.txtPort.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 110);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.lblFlag);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.Label lblStatus;
+        private System.ComponentModel.BackgroundWorker bgwReceiveData;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblFlag;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+using Gomoku;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        #region Atrributes
+        #region Thread
+        private Thread tRunning = null;
+        #endregion Thread
+
+        #region Clients data
+        private TcpClient client;
+
+        GomokuBoard board = new GomokuBoard();
+        GomokuTeam team = new GomokuTeam();
+        #endregion Clients data
+
+        #region Sender and Receiver
+        NetworkStream networkStream;
+        #endregion Sender and Receiver
+
+        #region Template Message
+        string received_message = "";
+        string prepared_message = "Ok";
+        string command;
+
+        const String get_name_incommand = "GetName";
+        const String set_flag_incommand = "Flag";
+        const String start_incommand = "Start";
+        const String stop_incommand = "Break";
+
+        const String team_name_outcommand = "TeamName:";
+        const String point_outcommand = "Point:";
+        const String donebreak_outcommand = "DoneBreak:";
+
+        #endregion Template Message
+
+        #region Default Port
+        int serverPort = 8001;
+        #endregion Default Port
+
+        #region Temporal Variables
+        int first_separate_index, second_separate_index, end_index;
+        #endregion Temporal_Variables
+        #endregion Attributes
+
+        #region initialization
+        public Form1()
+        {
+            InitializeComponent();
+
+            team.Team_Name = GetLetter().ToString(); //Ghi ten nhom cua cac em vao day nhe
+        }
+        #endregion initialization
+
+        #region Event
+        #region Button
+        /// <summary>
+        /// Button Connect Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint IP_End = new IPEndPoint(IPAddress.Parse(this.txtIP.Text), this.serverPort);
+
+            try
+            {
+                client.Connect(IP_End);
+
+                if (client.Connected)
+                {
+                    lblStatus.Text = "Connected";
+
+                    networkStream = this.client.GetStream();
+
+                    bgwReceiveData.RunWorkerAsync(); //start receiving data in background
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message.ToString());
+            }
+        }
+        #endregion Button
+
+        #region Background Worker
+        /// <summary>
+        /// Message Receiving thread
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bgwReceiveData_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    byte[] buffer = new byte[100];
+                    this.networkStream.Read(buffer, 0, buffer.Length);
+                    this.received_message = System.Text.Encoding.Default.GetString(buffer);
+
+                    if (received_message != null && received_message.Length > 0)
+                    {
+                        received_message_processing();
+                    }
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.ToString());
+                }
+            }
+        }
+        #endregion Background Worker
+
+        #region Form Event
+        /// <summary>
+        /// Form Closing Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopGame();
+        }
+        #endregion Form Event
+        #endregion Event
+
+        #region Method
+        #region Receiver and Sender
+        /// <summary>
+        /// decide to process when received the message
+        /// </summary>
+        private void received_message_processing()
+        {
+            first_separate_index = received_message.IndexOf(':');
+
+            if (first_separate_index <= 0)
+            {
+                return;
+            }
+
+            command = received_message.Substring(0, first_separate_index);
+
+            this.lblStatus.Invoke((MethodInvoker)delegate { this.lblStatus.Text = "Rec: " + received_message; });
+
+            if (command == get_name_incommand)
+            {
+                prepared_message = team_name_outcommand + team.Team_Name;
+
+                send_message();
+            }
+            else if (command == set_flag_incommand)
+            {
+                team.Flag = received_message.Substring(first_separate_index + 1)[0];
+                txtPort.Text = Convert.ToString(team.Flag);
+            }
+            else if (command == start_incommand)
+            {
+                get_opponent_turn();
+                StartClientThread();
+            }
+            else if (command == stop_incommand)
+            {
+                get_opponent_turn();
+                StopClientThread();
+
+                prepared_message = donebreak_outcommand;
+                send_message();
+            }
+        }
+
+        /// <summary>
+        /// Send Message
+        /// </summary>
+        public void send_message()
+        {
+            if (client.Connected)
+            {
+                byte[] tmp = Encoding.UTF8.GetBytes(this.prepared_message);
+                this.networkStream.Write(tmp, 0, prepared_message.Length);
+            }
+            else
+            {
+                MessageBox.Show("Fail...");
+            }
+        }
+        #endregion Receiver and Sender
+
+        #region Thread
+        /// <summary>
+        /// Bắt đầu thread liên lạc của client
+        /// </summary>
+        public void StartClientThread()
+        {
+            try
+            {
+                tRunning = new Thread(new ThreadStart(send_think_result));
+                tRunning.Start();
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Tắt thread liên lạc của client
+        /// </summary>
+        public void StopClientThread()
+        {
+            try
+            {
+                tRunning.Abort("Client: Terminated");
+                tRunning.Join();
+            }
+            catch {};
+        }
+
+        public void StopGame()
+        {
+            StopClientThread();
+        }
+        #endregion Thread
+
+        #region Perform The Turn
+        /// <summary>
+        /// Save your opponent turn
+        /// </summary>
+        public void get_opponent_turn()
+        {
+            Point p = new Point();
+
+            second_separate_index = received_message.LastIndexOf(':');
+            end_index = received_message.LastIndexOf('.');
+
+            p.X = Convert.ToInt32(received_message.Substring(first_separate_index + 1, second_separate_index - first_separate_index - 1));
+            p.Y = Convert.ToInt32(received_message.Substring(second_separate_index + 1, this.end_index - second_separate_index - 1));
+
+            if (p.X != -1 && p.Y != -1)
+            {
+                this.board.select(p, this.team.Flag);
+            }
+        }
+
+        /// <summary>
+        /// Send the result of your turn
+        /// </summary>
+        public void send_think_result()
+        {
+            Point p = new Point();
+
+            p = Think();
+
+            this.board.select(p, this.team.Flag);
+            prepared_message = point_outcommand + p.X + ":" + p.Y + ".";
+            send_message();
+
+            StopClientThread();
+        }
+        #endregion Perform The Turn
+
+        /// Cac em se tien hanh chinh sua code trong region Your Work nay
+        /// Cac region khac khong can chu tam den nhe
+        #region Your Work
+		int max_depth = 3;
+
+        /// <summary>
+        /// Think
+        /// </summary>
+        /// <returns></returns>
+        private Point Think()
+        {
+            Point result = board.getRandomFreeCell();
+            String text = result.X + "," + result.Y;
+            /*
+            string fileName = "C:\\txt\\" + team.Flag.ToString() + ".txt";
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine(text);
+            }*/
+            return result;
+        }
+
+        public static char GetLetter()
+        {
+	    // This method returns a random lowercase letter.
+	    // ... Between 'a' and 'z' inclusize.
+            Random _random = new Random();
+	        int num = _random.Next(0, 26); // Zero to 25
+	        char let = (char)('a' + num);
+	        return let;
+        }
+
+		struct Selected_cell {
+			public Point p;
+			public int v;
+		}
+
+		private int Valuate (GomokuBoard b) {
+			return 1;
+		}
+
+        private Selected_cell MaxValue(Selected_cell[] a) {
+            Selected_cell max = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (max.v < a[i].v)
+                    max = a[i];
+            }
+
+            return max;
+        }
+
+        private Selected_cell MinValue(Selected_cell[] a)
+        {
+            Selected_cell min = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (min.v > a[i].v)
+                    min = a[i];
+            }
+
+            return min;
+        }
+
+        private Selected_cell[] Available_cell(GomokuBoard b)
+        {
+			Selected_cell[] result = new Selected_cell[20];
+
+			return result;
+		}
+
+		private GomokuBoard Clone(GomokuBoard b) {
+			GomokuBoard result = new GomokuBoard ();
+
+			result.Board = b.Board;
+			result.Last_Selected_Position = b.Last_Selected_Position;
+
+			return result;
+		}
+
+		private Selected_cell Minimax(GomokuBoard b, int depth) {
+			return Max (b, depth);
+		}
+
+		private Selected_cell Max(GomokuBoard b, int depth) 
+		{
+			Selected_cell result;
+			Selected_cell[] a = Available_cell(b);
+			if (depth == max_depth) {
+
+				for (int i = 0; i < a.Length; i++) {
+					a[i].v = Valuate(b);
+                    
+				}
+                result = MaxValue(a);
+				return result;
+			}
+
+			List<Selected_cell> list_cell = new List<Selected_cell>();
+
+			foreach (Selected_cell x in a) {
+				GomokuBoard tmp = Clone(b);
+
+                tmp.select(x.p, this.team.Flag);
+				list_cell.Add(Max(tmp, depth + 1));
+
+                result = Min(tmp, depth + 1);
+			}
+            result = MaxValue(list_cell.ToArray());
+			return result;
+		}
+
+		private Selected_cell Min(GomokuBoard b, int depth) 
+		{
+            Selected_cell result;
+            Selected_cell[] a = Available_cell(b);
+            if (depth == max_depth)
+            {
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i].v = Valuate(b);
+
+                }
+                result = MaxValue(a);
+                return result;
+            }
+
+            List<Selected_cell> list_cell = new List<Selected_cell>();
+
+            foreach (Selected_cell x in a)
+            {
+                GomokuBoard tmp = Clone(b);
+
+                tmp.select(x.p, this.team.Flag);
+                list_cell.Add(Max(tmp, depth + 1));
+
+                result = Min(tmp, depth + 1);
+            }
+            result = MaxValue(list_cell.ToArray());
+            return result;
+		}
+
+
+
+
+        #endregion Your Work
+        #endregion Method
+        //ends--
+    }
+}
